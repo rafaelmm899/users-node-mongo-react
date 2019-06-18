ee5e5bafcdc8d80697cafa00dcdb5a52679627e9
Jose Mata <rafaelmm899@hotmail.com>
2019-06-17 23:30:38 -0400

    edit user in the same table

--- client/src/components/BuildTable.js
+++ client/src/components/BuildTable.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, Form } from "react-bootstrap";
 
 export class BuilTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: this.props.users
+        };
+    }
+
+    static defaultProps = {
+        users: []
+    };
+
     renderHeader(fields) {
         return (
             <tr>
@@ -16,30 +28,84 @@ export class BuilTable extends Component {
         );
     }
 
+    renderRow(user) {
+        return (
+            <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.lastname}</td>
+                <td>{user.email}</td>
+                <td>{user.country}</td>
+                <td>
+                    <Button
+                        variant="success"
+                        onClick={e => this.handleClickEdit(user, true)}
+                    >
+                        Edit
+                    </Button>
+                </td>
+                <td>
+                    <Button variant="danger">Delete</Button>
+                </td>
+            </tr>
+        );
+    }
+
+    handleClickEdit(dataUser, type) {
+        const { users } = this.state;
+        users.map(user => {
+            if (user._id === dataUser._id) {
+                user.edit = type;
+            }
+            return type;
+        });
+
+        this.setState({
+            users
+        });
+    }
+
+    renderFormEdit(user) {
+        return (
+            <tr key={user._id}>
+                <td>
+                    <Form.Control type="text" defaultValue={user.name} />
+                </td>
+                <td>
+                    <Form.Control type="text" defaultValue={user.lastname} />
+                </td>
+                <td>
+                    <Form.Control type="text" defaultValue={user.email} />
+                </td>
+                <td>
+                    <Form.Control type="text" defaultValue={user.country} />
+                </td>
+                <td>
+                    <Button variant="success">Save</Button>
+                </td>
+                <td>
+                    <Button
+                        variant="danger"
+                        onClick={e => this.handleClickEdit(user, false)}
+                    >
+                        Cancel
+                    </Button>
+                </td>
+            </tr>
+        );
+    }
+
     renderTableData(users) {
         return users.map(user => {
-            return (
-                <tr key={user._id}>
-                    <td>{user.name}</td>
-                    <td>{user.lastname}</td>
-                    <td>{user.email}</td>
-                    <td>{user.country}</td>
-                    <td>
-                        <Button variant="success">Edit</Button>
-                    </td>
-                    <td>
-                        <Button variant="danger">Delete</Button>
-                    </td>
-                </tr>
-            );
+            return user.edit ? this.renderFormEdit(user) : this.renderRow(user);
         });
     }
 
     render() {
+        const { users } = this.state;
         return (
             <Table responsive>
                 <thead>{this.renderHeader(this.props.headers)}</thead>
-                <tbody>{this.renderTableData(this.props.users)}</tbody>
+                <tbody>{this.renderTableData(users)}</tbody>
             </Table>
         );
     }
